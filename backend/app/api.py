216b45280bf0ab2935bc030a5199dce4f59ca37f
Rhythem Agrawal <rhythemagrawal@gmail.com>
2026-04-13 16:12:39 +0530

    Add token refresh endpoint

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, Response, Cookie
 from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
-from sqlalchemy import select
+from sqlalchemy import select, update
 from typing import Annotated
 from argon2 import PasswordHasher
 from datetime import datetime, timedelta, timezone
@@ -174,5 +174,66 @@ def logout(response: Response, refresh_token: Annotated[str | None, Cookie()] = 
     return
 
 @router.post("/refresh", response_model=LoginResponse)
-def refresh(refresh_token: Annotated[str | None, Cookie()]):
+def refresh(refresh_token: Annotated[str | None, Cookie()] = None, db: Session = Depends(get_db)) -> JSONResponse:
+    if not refresh_token:
+        raise HTTPException(status_code=401, detail="Invalid refresh token")
     
+    hashed_refresh_token = hashlib.sha256(refresh_token.encode()).hexdigest()
+    token_query = select(RefreshToken).where(RefreshToken.hashed_token == hashed_refresh_token)
+    results = db.execute(token_query).scalars().all()
+
+    if not results:
+        raise HTTPException(status_code=401, detail="Invalid refresh token")
+    
+    db_token = results[0]
+    db_user = db_token.user
+
+    if db_token.revoked or db_token.expires_at < datetime.now(timezone.utc):
+        revoke_query = update(RefreshToken)\
+            .where(RefreshToken.user_id == db_user.id)\
+            .values(revoked=True)
+        db.execute(revoke_query)
+        db.commit()
+
+        raise HTTPException(status_code=401, detail="Refresh token expired")
+
+    content = UserRead.model_validate(db_user).model_dump()
+
+    payload = {
+        "sub": str(db_user.id),
+        "iat": int(time.time()),
+        "exp": int(time.time()) + settings.jwt_access_token_expires_seconds
+    }
+
+    private_key = Path(settings.jwt_private_key_path).read_text()
+    token = jwt.encode(payload, private_key, algorithm=settings.jwt_algorithm)
+    content.update({
+        "access_token": token,
+        "token_type": "bearer"
+    })
+
+    response = JSONResponse(content=content)
+    refresh_token = secrets.token_urlsafe(32)
+    hashed_refresh_token = hashlib.sha256(refresh_token.encode()).hexdigest()
+
+    response.set_cookie(
+        key="refresh_token",
+        value=refresh_token,
+        httponly=True,
+        secure=True,
+        samesite="strict"
+    )
+
+    db_refresh_token = RefreshToken(
+        user_id=db_user.id,
+        hashed_token=hashed_refresh_token,
+        expires_at=datetime.now(timezone.utc) + timedelta(days=30),
+        created_at=datetime.now(timezone.utc),
+        revoked=False
+    )
+
+    db.add(db_refresh_token)
+    db_token.revoked = True
+    db.commit()
+
+    return response
